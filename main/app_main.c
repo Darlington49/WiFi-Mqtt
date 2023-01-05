c218b3fd8ed1493b8096bc20b746b6952bf27b70
Youcef <youce.31@gmail.com>
2023-01-05 14:44:31 +0100

    fix bugs

--- main/app_main.c
+++ main/app_main.c
@@ -31,6 +31,8 @@
 #include "esp_log.h"
 #include "mqtt.h"
 #include "main.h"
+#include "WiFi.h"
+
 static const char *TAG = "Main";
 
 EventGroupHandle_t connectivity_event_group;
@@ -40,10 +42,6 @@ EventGroupHandle_t connectivity_event_group;
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT BIT1
 #define MQTT_CONNECTED BIT2
-
-#define EXAMPLE_ESP_WIFI_SSID "AndroidAP9CB9"
-#define EXAMPLE_ESP_WIFI_PASS "00000000"
-#define EXAMPLE_ESP_MAXIMUM_RETRY 5
 
 xQueueHandle MqttPublishQueue;
 
@@ -60,7 +58,7 @@ int vprintf_into_spiffs(const char *szFormat, va_list args)
     if (ret >= 0)
     {
         // // debug output
-        // printf(" ppp \t[Writing to SPIFFS] %.*s", ret, log_print_buffer);
+        printf(" ppp \t[Writing to SPIFFS] %.*s", ret, log_print_buffer);
         // int msg_id = esp_mqtt_client_publish(client, "/5onr/log", &log_print_buffer, 0, 1, 0);
         // printf("sent publish successful, msg_id=%d", msg_id);
 
@@ -76,7 +74,7 @@ int vprintf_into_spiffs(const char *szFormat, va_list args)
         }
         else
         {
-            printf("MqttPublishQueue:  failed to add message to queue");
+            printf("MqttPublishQueue:  failed to add message to queue\n");
             // break;
         }
     }
@@ -113,102 +111,33 @@ void task2(void *params)
     }
 }
 
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
-    {
-        esp_wifi_connect();
-        ESP_LOGI(TAG, "WIFI_EVENT_STA_START");
-    }
-    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
-    {
-        xEventGroupClearBits(connectivity_event_group, WIFI_CONNECTED_BIT);
-        ESP_LOGI(TAG, "Wi-Fi disconnected, trying to reconnect...");
-        esp_wifi_connect();
-    }
-    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
-    {
-        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        xEventGroupSetBits(connectivity_event_group, WIFI_CONNECTED_BIT);
-        mqttStart();
-    }
-}
-
-void wifi_init_sta(void)
-{
-    // ESP_ERROR_CHECK(esp_netif_init());
-
-    // ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = EXAMPLE_ESP_WIFI_SSID,
-            .password = EXAMPLE_ESP_WIFI_PASS,
-            /* Setting a password implies station will connect to all security modes including WEP/WPA.
-             * However these modes are deprecated and not advisable to be used. Incase your Access point
-             * doesn't support WPA2, these mode can be enabled by commenting below line */
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-
-            .pmf_cfg = {
-                .capable = true,
-                .required = false},
-        },
-    };
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
-
-    ESP_LOGI(TAG, "wifi_init_sta finished.");
-}
-
 void app_main(void)
 {
     ESP_LOGI(TAG, "[APP] Startup..");
     ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
     ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
-
+    // esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT", ESP_LOG_WARN);
+    esp_log_level_set("MqttPublishQueue", ESP_LOG_WARN);
+    esp_log_level_set("Main", ESP_LOG_VERBOSE);
+    esp_log_level_set("WiFi", ESP_LOG_VERBOSE);
+    // esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+    // esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+    // esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    // esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+    esp_log_level_set("wifi", ESP_LOG_WARN);
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     wifi_init_sta();
-    // /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-    //  * Read "Establishing Wi-Fi or Ethernet Connection" section in
-    //  * examples/protocols/README.md for more information about this function.
-    //  */
-    // ESP_ERROR_CHECK(example_connect());
-    // esp_log_set_vprintf(&vprintf_into_spiffs);
-    connectivity_event_group = xEventGroupCreate();
-    MqttPublishQueue = xQueueCreate(20, sizeof(payload_t));
 
-    // xTaskCreate(&MqttPublisherTask, "do something with http", 3048, NULL, 1, NULL);
-    // xTaskCreate(&task2, "Simulate", 3048, NULL, 1, NULL);
+    esp_log_set_vprintf(&vprintf_into_spiffs);
+    connectivity_event_group = xEventGroupCreate();
+    MqttPublishQueue = xQueueCreate(30, sizeof(payload_t));
+
+    xTaskCreate(&MqttPublisherTask, "do something with http", 3048, NULL, 1, NULL);
+    xTaskCreate(&task2, "Simulate", 3048, NULL, 1, NULL);
 
     mqttInit();
 }
